fae4fdd5a0ecdce0f967e824787f936073e7c950
Lautaro Arce <lautaroarce89@gmail.com>
2026-09-07 17:04:16 -0300

    agregar reinicio automatico del contenedor

--- app/deploy.sh
+++ app/deploy.sh
@@ -4,4 +4,4 @@ git pull
 sudo docker build -t vpc-web .
 sudo docker stop vpc-web-container
 sudo docker rm vpc-web-container
-sudo docker run -d --name vpc-web-container -p 80:80 vpc-web
+sudo docker run -d --restart unless-stopped --name vpc-web-container -p 80:80 vpc-web

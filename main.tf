35a24f9520a282ec31f73a7f369c138ff5eaeb45
Lautaro Arce <lautaroarce89@gmail.com>
2026-09-15 19:21:23 -0300

    agregar repositorio ecr y permisos iam

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_key_pair" "vpc_lab" {
 }
 
 resource "aws_instance" "publica" {
-  ami           = data.aws_ami.amazon_linux_2023.id
+  ami           = "ami-007dd4cdc89d5d91d"
   instance_type = "t3.micro"
 
   subnet_id = aws_subnet.publica.id
@@ -242,6 +242,86 @@ resource "aws_iam_role_policy" "github_actions_ssm" {
         ]
 
         Resource = "*"
+      }
+    ]
+  })
+}
+
+#creamos un repositorio de Amazon Elastic Container Registry (ECR) llamado "vpc-web". Este repositorio se utiliza para almacenar imágenes de contenedores Docker. La configuración incluye la mutabilidad de las etiquetas de imagen (permitiendo que las etiquetas puedan cambiar), la habilitación del escaneo de imágenes al hacer push (para detectar vulnerabilidades) y la asignación de etiquetas para identificar el recurso en AWS.
+resource "aws_ecr_repository" "vpc_web" {
+  name                 = "vpc-web"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "vpc-web"
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "github-actions-ecr"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+
+        Resource = aws_ecr_repository.vpc_web.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_ecr" {
+  name = "ec2-ecr-read"
+  role = aws_iam_role.ec2_ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+
+        Resource = aws_ecr_repository.vpc_web.arn
       }
     ]
   })
